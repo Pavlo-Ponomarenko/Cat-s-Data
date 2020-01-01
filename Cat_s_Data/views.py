5e09748f98b0e215c3e3703326b2c04d1d9b8c12
Pavlo Ponomarenko <reactor2345@gmail.com>
2020-01-01 20:20:45 +0300

    Создана пробная web-страница, удалены ненужные сгенерированые елементы проекта

--- Cat_s_Data/views.py
+++ Cat_s_Data/views.py
@@ -2,8 +2,7 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from Cat_s_Data import app
 
 @app.route('/')
@@ -12,26 +11,6 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        title='Cat\'s Data'
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
