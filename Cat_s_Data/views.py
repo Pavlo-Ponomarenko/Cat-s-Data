ab7e6e1bb32662aeec90cf3118d4ea931a1728ca
Pavlo Ponomarenko <reactor2345@gmail.com>
2020-01-01 23:44:58 +0300

    Реализована функция сохранения данных

--- Cat_s_Data/views.py
+++ Cat_s_Data/views.py
@@ -18,8 +18,22 @@ def data_getting():
     file = open('data.pkl', 'rb')
     data = pickle.load(file)
     file.close()
-    our_request = request.form['Input_id']
+    our_request = request.form['Input_id'].strip()
     if our_request in data:
-        return render_template('index.html', title='Cat\'s Data', x=data[our_request])
+        return render_template('index.html', title='Cat\'s Data', x=data[our_request], id_into=our_request)
     return render_template('index.html', title='Cat\'s Data', message="Данных не обнаружено")
-    
+
+@app.route('/data_add', methods=['POST'])
+def data_adding():
+    file = open('data.pkl', 'rb')
+    data = pickle.load(file)
+    file.close()
+    our_request = request.form['Input_id']
+    if our_request != "" and not (' ' in list(our_request.strip())):
+        data[our_request] = request.form["TextArea"]
+        file = open('data.pkl', 'wb')
+        pickle.dump(data, file)
+        file.close()
+        return ('', 204)
+    else:
+        return render_template('index.html', title='Cat\'s Data', message="Не корректное id")
