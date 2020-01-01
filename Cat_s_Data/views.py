be57a14321b6328f9828484f239c630b81a6c5f4
Pavlo Ponomarenko <reactor2345@gmail.com>
2020-01-01 21:49:00 +0300

    Реализована функция поиска по id

--- Cat_s_Data/views.py
+++ Cat_s_Data/views.py
@@ -1,7 +1,7 @@
 """
 Routes and views for the flask application.
 """
-
+import pickle
 from flask import render_template, request
 from Cat_s_Data import app
 
@@ -11,6 +11,15 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Cat\'s Data'
-    )
+        title='Cat\'s Data')
 
+@app.route('/data_rec', methods=['POST'])
+def data_getting():
+    file = open('data.pkl', 'rb')
+    data = pickle.load(file)
+    file.close()
+    our_request = request.form['Input_id']
+    if our_request in data:
+        return render_template('index.html', title='Cat\'s Data', x=data[our_request])
+    return render_template('index.html', title='Cat\'s Data', message="Данных не обнаружено")
+    
